2100207c335e2dff543785707c7d6e71db835a5b
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-02-27 01:05:31 -0300

    NoteController criado

---
+++ src/main/java/com/luizx/todolist/controller/NoteController.java
@@ -0,0 +1,48 @@
+package com.luizx.todolist.controller;
+
+import com.luizx.todolist.model.Note;
+import com.luizx.todolist.services.NoteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+
+    private final NoteService service;
+
+    public NoteController(NoteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> insertNewNote(@RequestBody Note note){
+        service.insertNewNote(note);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Note>> findAllNotes(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAllNotes());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> findById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteNote(@PathVariable Long id){
+        service.deleteNoteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteAllNotes(){
+        service.deleteAllNotes();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
