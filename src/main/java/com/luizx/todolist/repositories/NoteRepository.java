eefa5ad5cf01796fbfe12ce0653c24749ff6f252
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-02-27 01:05:03 -0300

    NoteRepository criado

---
+++ src/main/java/com/luizx/todolist/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.luizx.todolist.repositories;
+
+import com.luizx.todolist.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
