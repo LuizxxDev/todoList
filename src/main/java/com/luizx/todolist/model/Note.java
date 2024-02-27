80b80389903d54d6b5ca2d9018b4df571ab5b232
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-02-27 20:58:19 -0300

    variavel date adicionada

--- src/main/java/com/luizx/todolist/model/Note.java
+++ src/main/java/com/luizx/todolist/model/Note.java
@@ -1,6 +1,12 @@
 package com.luizx.todolist.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "note")
@@ -10,14 +16,30 @@ public class Note {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Length(min = 1, max = 30)
+    @NotBlank(message = "O campo title não pode ser nulo ou vazio")
     private String title;
 
+    @Length(min = 1)
+    @NotBlank(message = "O campo description não pode ser nulo ou vazio")
     private String description;
+
+    @Column(nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
+    private LocalDateTime date;
 
     @ManyToOne
     private Category category;
 
     public Note(){
+    }
+
+    public Note(Long id, String title, String description, Category category, LocalDateTime date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.date = date;
     }
 
     public Long getId() {
@@ -50,5 +72,13 @@ public class Note {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime dateTime) {
+        this.date = dateTime;
     }
 }
