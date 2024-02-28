0bbe349e04e3e2bba47b0c3acdd1dd2e6f13b254
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-02-27 21:00:37 -0300

    validations bin incluidas

--- src/main/java/com/luizx/todolist/model/Category.java
+++ src/main/java/com/luizx/todolist/model/Category.java
@@ -1,17 +1,28 @@
 package com.luizx.todolist.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
-@Table(name = "tb_categories")
+@Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    @Length(min = 1, max = 30)
+    @NotBlank(message = "O campo name não pode ser nulo ou vazio")
     private String name;
 
     public Category(){
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Long getId() {
